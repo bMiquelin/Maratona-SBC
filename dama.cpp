49fc04df604e934c2f025a59006674fbbacd4242
Bruno Miquelin <bruno.b.miquelin@gmail.com>
2015-09-19 11:09:47 -0300

    Update dama.cpp

--- dama.cpp
+++ dama.cpp
@@ -17,7 +17,7 @@ int main()
 		
 		if (x1 == x2 && y1 == y2)
 			cout << 0 << endl;
-		else if (x1 == x2 || y1 == y2 || x1+y1 == x2+y2)
+		else if (x1 == x2 || y1 == y2 || x1+y1 == x2+y2 || y1-x1 == y2-x2)
 			cout << 1 << endl;
 		else
 			cout << 2 << endl;			
